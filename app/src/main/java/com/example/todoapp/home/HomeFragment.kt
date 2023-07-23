cc39c9bde7e90335b856bd7f16c411fd253adfcf
thcainthry <hasani.florinda@gmail.com>
2023-07-23 23:02:50 +0200

    started new Todo App.

---
+++ app/src/main/java/com/example/todoapp/home/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.example.todoapp.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.todoapp.databinding.FragmentHomeBinding
+
+
+class HomeFragment: Fragment() {
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
